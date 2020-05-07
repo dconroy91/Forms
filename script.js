8fa7bd34d08d52c14781fd7d6521bc051d19cdf5
Devin Conroy <d.conroy1991@gmail.com>
2020-05-07 14:33:14 -0400

    update repository

---
+++ script.js
@@ -0,0 +1,30 @@
+// const darkModeButton = document.querySelector("#dark-mode")
+
+// console.log(darkModeButton)
+
+// darkModeButton.addEventListener("click", 
+// function(){
+//     const formElement = document.querySelector("form");
+//     formElement.classlist.toggle("dark-mode")
+// })
+
+// document.querySelector("#jordan-mode-button").addEventListener("click", 
+// function(){
+//     document.querySelector("form").classList.toggle("jordan-mode")
+// }
+// )
+
+// document.querySelector("#submit-button").addEventListener("Click", function(){
+//     const nameInput = document.querySelector("#name-input")
+// })
+
+// document.querySelector("#message").addEventListener("keyup", function () {
+//     const messageInput = document.querySelector("#message")
+
+//     const messageValue = messageInput.value;
+
+//     document.querySelector("#art-1").textContent = messageValue
+
+//     document.querySelector("#art-2").textContent = messageValue
+
+// })
